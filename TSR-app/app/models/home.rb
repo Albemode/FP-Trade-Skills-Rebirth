3a160537d0697cf4fe166982e0fd9562c7e9a1ca
Alberto Modesto <albe.mode.24@gmail.com>
2016-12-08 16:47:08 -0500

    sign form works

---
+++ TSR-app/app/models/home.rb
@@ -0,0 +1,5 @@
+class Home < ApplicationRecord
+  def index
+
+  end
+end
--- TSR-app/config/routes.rb
+++ TSR-app/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :apprentices
+  devise_for :masters
+  resources :homes
+  resources :skills
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "homes#index"
+
+  # skills
+  
 end
